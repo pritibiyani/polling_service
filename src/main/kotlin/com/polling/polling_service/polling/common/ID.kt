3777c8bc076bc6054c1175f81ab70f1d9507809c
Priti <priti.biyani6@gmail.com>
2018-09-04 19:34:05 +0530

    Added ID generator by time and name.

---
+++ src/main/kotlin/com/polling/polling_service/polling/common/ID.kt
@@ -0,0 +1,20 @@
+package com.polling.polling_service.polling.common
+
+import java.time.LocalDateTime
+import java.util.*
+
+
+class UniqueIdGenerator{
+    companion object Factory{
+        val nameSpace = "some-namespace"
+
+        fun byName(name: String): String {
+            val string = nameSpace + name + LocalDateTime.now()
+            return UUID.nameUUIDFromBytes(string.toByteArray()).toString()
+        }
+
+        fun byTime(): String {
+            return UUID.randomUUID().toString();
+        }
+    }
+}
